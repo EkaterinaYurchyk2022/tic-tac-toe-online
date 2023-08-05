161351be426251efeefcb2e6b6895ec125ec36b3
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-08-05 13:42:29 +0300

    feat: uikit, button component; game field style: button, game field

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -2,15 +2,17 @@ import Image from "next/image";
 import logoSrc from "./logo.svg";
 import { Profile } from "../profile";
 import { ArrowDownIcon } from "./icons/ArrowDownIcon";
+import { UiButton } from "../uikit/UiButton";
 
 export function Header() {
   return (
     <header className="flex h-24 px-8 items-center bg-white shadow-lg">
       <Image src={logoSrc} alt="logo" />
       <div className="w-px h-8 bg-slate-200 mx-6" />
-      <button className="w-44 bg-teal-600 transition-colors text-white rounded-lg px-5 py-2 text-2xl leading-tight hover:bg-teal-500">
+      <UiButton className="w-44" size="lg" variant="primary">
         Играть
-      </button>
+      </UiButton>
+
       <button className="ml-auto flex items-center gap-2 text-teal-600">
         <Profile />
         <ArrowDownIcon />
